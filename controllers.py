0dae2e77ff44f0832643ea051ac46daee0181a0b
skyfall947 <atencio947@gmail.com>
2019-06-02 12:21:19 -0400

    v0.0.3

--- controllers.py
+++ controllers.py
@@ -1,9 +1,12 @@
 from flask_restful import Resource
 from flask import request, jsonify
 from numpy import *
+import numpy as np
 import pandas as pd
-global message
 
+from scipy import stats
+global message
+global name
 
 def clean(a):
     aux=[]
@@ -36,10 +39,22 @@ def resta(a,b):
 def multiplicacion(a,b):
     try:
         global message 
-        message = 'Se realizo la multiplicacion correctamente'
+        message = 'Se realizo el producto vectorial correctamente'
         return a*b
     except:
         message = '(Error) No se cumple la condicion de que las columnas de la matriz A sea igual a las filas de la matriz B. shape(a)={} shape(b)={}'.format(shape(a), shape(b))
+def producto_escalar(a,b):
+    try:
+        global message 
+        message = 'Se realizo el producto escalar correctamente'
+        res=0;a=array(a);b=array(b)
+        for i in range(a.shape[0]):
+            for j in range(a.shape[1]):
+                res+= a[i][j]*b[i][j]
+        return [res]
+    except:
+        message = '(Error) Las dimensiones de sus matrices no son iguales shape(a)={} shape(b)={} '.format(shape(a), shape(b))
+
 
 def transpuesta(a):
     try:
@@ -104,6 +119,30 @@ def lineal(a,b):
     except :
         message = '(Error) Ocurrio un error al resolver el sistema. shape(A)={} shape(B)={} '.format(shape(a), shape(b))
 
+def arrayNormal(m,n):
+    global message, name
+    message =  'Se creo el archivo correctamente'
+    name = 'Distribucion Normal Scipy'
+    f = stats.norm.pdf
+    v_bound = np.sqrt(f(np.sqrt(2))) * np.sqrt(2)
+    umax, vmin, vmax = np.sqrt(f(0)), -v_bound, v_bound
+    rvs = []
+    for i in range(m):
+        print(n)
+        rvs.append(stats.rvs_ratio_uniforms(f, umax, vmin, vmax, size=n))
+    return rvs
+def arrayMulti(m,n):
+    global message, name
+    message =  'Se creo el archivo correctamente'
+    name = 'Multivariate_normal Scipy'
+    f = stats.multivariate_normal.pdf
+    v_bound = np.sqrt(f(np.sqrt(2))) * np.sqrt(2)
+    umax, vmin, vmax = np.sqrt(f(0)), -v_bound, v_bound
+    rvs = []
+    for i in range(m):
+        print(n)
+        rvs.append(stats.rvs_ratio_uniforms(f, umax, vmin, vmax, size=n))
+    return rvs
 class Employees(Resource):
     def get(self):
         return 'Servicio funcionando :D'
@@ -120,6 +159,7 @@ class Operacion(Resource):
             if op == '-': res = resta(a,b)
             if op == '*': res = multiplicacion(a,b)
             if op =='lineal': res = lineal(a,b)
+            if op == 'escalar': res = producto_escalar(a,b)
             res = pd.DataFrame(res).to_json( orient='split')
             return {'resultado': res, 'mensaje': message}
 
@@ -143,14 +183,33 @@ class Unimatriz(Resource):
             res = pd.DataFrame(res).to_json( orient='split')
             
             return {'resultado': res, 'mensaje': message}
-
+ 
 class Random(Resource):
     def post(self):
-        global message
+        global message, name
         if request.method == 'POST':
             resp = request.get_json()
-            print(resp)
             m = int(resp['m']); n = int(resp['n'])
-            res = random.randint(101, size=(m, n))
+            a = int(resp['a']); b = int(resp['b']); tipo = resp['type']; 
+            if tipo == 'float':
+                message='Se creo la tabla de excel de valores de tipo float con los siguientes valores: filas:{}, columnas:{}, rango: ({}, {}) '.format(m,n,a,b)
+                name='Float({}, {})'.format(n,m)
+                res = (1+b-a) * np.random.random_sample((m, n)) + a
+            if tipo == 'normal':
+                name='Normal({}, {})'.format(n,m)
+                mu = resp['mu']; sigma = resp['sigma']
+                message='Se creo la tabla de excel de distribucion normal con los siguientes valores: filas:{}, columnas:{}, mu:{}, sigma:{} '.format(m,n,mu,sigma)
+                res=[]
+                for i in range(m): 
+                    res.append(np.random.normal(mu, sigma, n))
+            if tipo=='normal_Scipy': res = arrayNormal(m,n)
+            if tipo=='Multivariate_normal': res = arrayMulti(m,n)
+            if tipo =='Integer':
+                name='Integers({}, {})'.format(n,m)
+                res = np.random.randint(a,b+1, size=(m, n))
+                message='Se creo la tabla de excel de valores de tipo int con los siguientes valores: filas:{}, columnas:{}, rango: ({}, {}) '.format(m,n,a,b)
             res = pd.DataFrame(res).to_json( orient='split')
-            return {'data': res}
+            return {'data': res, 'message': message, 'name': name}
+
+
+
